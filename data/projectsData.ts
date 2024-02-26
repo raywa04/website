c46c3bf59ae8c0f6feab4bb66db021d397e5107f
Rayyan Waris <raywa04@gmail.com>
2024-02-26 13:02:37 -0600

    added my webscrapping and chatbot to my website

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -21,6 +21,24 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/epsoft.png',
     href: 'https://github.com/raywa04/Customer-Assistance-Bot',
   },
+  {
+    title: 'Data Chart Automation Bot',
+    description: `Harnessing the power of Java and Apache Maven, we've crafted a dynamic program designed to revolutionize the way charts, graphs, 
+    and tables are updated within PowerPoint presentations. Our innovative solution not only streamlines the process but also ensures unparalleled 
+    accuracy and relevance, elevating the quality of your presentations to new heights. Say goodbye to manual updates and hello to efficiency with 
+    our cutting-edge automated solution. `,
+    imgSrc: '/static/images/powerpoint.png',
+    href: 'https://github.com/raywa04/Data-Chart-Automation-Bot',
+  },
+  {
+    title: 'Web Scraping Bot',
+    description: `Powered by Python, Beautiful Soup, and the OpenAI API, our innovative tool is designed to streamline the process of gathering and
+    summarizing web content. Leveraging the capabilities of Beautiful Soup, we meticulously navigate website structures to retrieve primary content 
+    with precision. This content is seamlessly inputted into the OpenAI GPT-3 language model, where it undergoes sophisticated processing to generate 
+    comprehensive summaries of websites. Say goodbye to manual content extraction and hello to efficient, automated summarization.`,
+    imgSrc: '/static/images/gpt3.png',
+    href: 'https://github.com/raywa04/Web-Scraping-Bot',
+  },
 ]
 
 export default projectsData
