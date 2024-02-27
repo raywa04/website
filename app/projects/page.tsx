7af187451b4bf273cdb99a36c0ff9670a7f81112
Rayyan Waris <raywa04@gmail.com>
2024-02-27 00:42:29 -0600

    fix issue with accessing blogs

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            A showcase of technical projects I've developed over the years.
           </p>
         </div>
         <div className="container py-12">
