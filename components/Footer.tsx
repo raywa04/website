222f93797f18e468c6ee5f663020f7e8bfcdc121
Rayyan Waris <raywa04@gmail.com>
2024-02-27 14:24:54 -0600

    fix bug from prev deploy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,9 +24,6 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          
-            
-          
         </div>
       </div>
     </footer>
